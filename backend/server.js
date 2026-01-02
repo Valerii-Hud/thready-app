274c548b157e21f88b70ca577982d22bbbd70531
Valerii Hud <support@valerii-hud.com>
2026-01-02 15:58:52 +0100

    feat: added notifications routes & notifications controllers

--- backend/server.js
+++ backend/server.js
@@ -3,9 +3,10 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 import connectToMongoDB from './database/connectToMongoDB.js';
-import authRoutes from './routes/auth.routes.js';
-import userRoutes from './routes/users.routes.js';
-import postRoutes from './routes/post.routes.js';
+import authRoutes from './routes/auth.route.js';
+import userRoutes from './routes/users.route.js';
+import postRoutes from './routes/post.route.js';
+import notificationRoutes from './routes/notification.route.js';
 
 import { v2 as cloudinary } from 'cloudinary';
 dotenv.config({
@@ -29,6 +30,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/posts', postRoutes);
+app.use('/api/notifications', notificationRoutes);
 
 app.listen(PORT, () => {
   connectToMongoDB();
