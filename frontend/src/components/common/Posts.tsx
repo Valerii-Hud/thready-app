74d89c9bdc71a394abb58610ecd3e6d4afa04353
Valerii Hud <support@valerii-hud.com>
2026-01-04 12:20:28 +0100

    feat: get posts in home page

--- frontend/src/components/common/Posts.tsx
+++ frontend/src/components/common/Posts.tsx
@@ -1,25 +1,67 @@
 import Post from './Post';
 import PostSkeleton from '../skeletons/PostSkeleton';
-import { POSTS } from '../../utils/database/dummy';
+import { useQuery } from '@tanstack/react-query';
+import type { iPost } from '../../types';
+import { useEffect } from 'react';
 
-const Posts = () => {
-  const isLoading = false;
+type tPostsProps = {
+  feedType: string;
+};
 
+const Posts = ({ feedType }: tPostsProps) => {
+  const getPostEndpoint = () => {
+    switch (feedType) {
+      case 'forYou':
+        return '/api/posts/all';
+      case 'following':
+        return '/api/posts/following';
+      default:
+        return '/api/posts/all';
+    }
+  };
+
+  const POST_ENDPOINT = getPostEndpoint();
+
+  const {
+    data: posts,
+    isPending,
+    refetch,
+    isRefetching,
+  } = useQuery({
+    queryKey: ['posts'],
+    queryFn: async () => {
+      try {
+        const response = await fetch(POST_ENDPOINT);
+        const data = await response.json();
+
+        if (!response.ok) {
+          throw new Error(data.error || 'Something went wrong');
+        }
+        return data;
+      } catch (error: unknown) {
+        if (error instanceof Error) throw new Error(error.message);
+      }
+    },
+  });
+
+  useEffect(() => {
+    refetch();
+  }, [feedType, refetch]);
   return (
     <>
-      {isLoading && (
+      {(isPending || isRefetching) && (
         <div className="flex flex-col justify-center">
           <PostSkeleton />
           <PostSkeleton />
           <PostSkeleton />
         </div>
       )}
-      {!isLoading && POSTS?.length === 0 && (
+      {!(isPending || isRefetching) && posts?.length === 0 && (
         <p className="text-center my-4">No posts in this tab. Switch 👻</p>
       )}
-      {!isLoading && POSTS && (
+      {!(isPending || isRefetching) && posts && (
         <div>
-          {POSTS.map((post) => (
+          {posts.map((post: iPost) => (
             <Post key={post._id} post={post} />
           ))}
         </div>
